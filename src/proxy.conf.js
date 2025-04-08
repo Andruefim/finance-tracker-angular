27f6252958661a499042e70b2f85971017f050df
Andrew Yefymenko <andruefim@gmail.com>
2025-04-08 19:23:48 +0200

    Configure to run with nestjs

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,15 +1,16 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7040';
+//const target = process.env.BACKEND_URL ?? env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7040';
+
 
 const PROXY_CONFIG = [
   {
     context: [
       "/api"
     ],
-    target,
-    secure: false
+    target: "http://localhost:7040/api",
+    secure: false,"start": "ng serve --proxy-config src/proxy.conf.js"
   }
 ]
 
